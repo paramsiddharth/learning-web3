24fc887538556db055f8ac44f6b8efa45e0f2f89
Param Siddharth <contact@paramsid.com>
2022-11-14 21:34:35 +0530

    Add deployed token address
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- 3-custom-crypto/contracts/batlu.sol
+++ 3-custom-crypto/contracts/batlu.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.1;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
+// Address: 0x42d4F8dB8ad1F7CB9CAC31d0F448BA8718D244cD
 contract BatluToken is ERC20 {
 	constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
 		_mint(msg.sender, 10 * 10 ** 18);
